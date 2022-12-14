190f84cfd149e81907f15d078e52826049439077
VasiliiOrlov <ov_prog@mail.ru>
2022-12-14 03:02:01 +0300

    Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/service/PostService.java
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -1,16 +1,18 @@
 package ru.job4j.dreamjob.service;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
 import java.util.Collection;
 
+@Service
 public class PostService {
 
-    private final static PostService INST = new PostService();
-    private final PostStore postStore = PostStore.instOf();
-    public static PostService instOf() {
-        return INST;
+    private final PostStore postStore;
+
+    public PostService(PostStore postStore) {
+        this.postStore = postStore;
     }
 
     public Collection<Post> findAll() {
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.store;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Post;
 
 import java.util.Collection;
@@ -7,9 +8,8 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class PostStore {
-
-    private static final PostStore INST = new PostStore();
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
@@ -19,10 +19,6 @@ public class PostStore {
         posts.put(1, new Post(1, "Junior Java Job", "little interesting"));
         posts.put(2, new Post(2, "Middle Java Job", "interesting"));
         posts.put(3, new Post(3, "Senior Java Job", "very interesting"));
-    }
-
-    public static PostStore instOf() {
-        return INST;
     }
 
     public void add(Post post) {
